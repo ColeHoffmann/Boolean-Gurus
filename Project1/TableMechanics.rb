b28399606add5dd1883cf397f2ef4da1337c26ac
Colby Sherwood <colbysherwood44@gmail.com>
2020-01-30 00:50:07 -0500

    Wrote first version of initialization

--- Project1/TableMechanics.rb
+++ Project1/TableMechanics.rb
@@ -1,4 +1,12 @@
 class Table
   def initialize
-
+	@table = Array.new
+	for i in 1..3
+		@tableRow = Array.new
+		for j in 1..4
+			@tableRow.push(Card.drawRandom)
+		end
+		@table.push(@tableRow)
+	end
+  end
 end
