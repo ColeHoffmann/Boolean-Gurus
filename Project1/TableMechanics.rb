3655670a11328e1d359df6052241ca47df2b6a7b
Cole Hoffmann <hoffmannOSU@gmail.com>
2020-01-31 16:51:29 -0500

    Touched up on the changed cards method

--- Project1/TableMechanics.rb
+++ Project1/TableMechanics.rb
@@ -21,8 +21,12 @@ def putTable
 
 
   
-
-  def changeChards(card1, card2, card3)
+  #These values should be THE SPOT IN THE ARRAY IN WHICH THE CARD RESIDES - "The i value" 
+  #For example, if cards array[3] array[6] and array[0] gets picked, the parameters would be 3, 6, and 0
+  def changeChards(currentTable, card1, card2, card3)
+    currentTable[card1] = Card.drawRandom
+    currentTable[card2] = Card.drawRandom
+    currentTable[card3] = Card.drawRandom
   end
     
 
