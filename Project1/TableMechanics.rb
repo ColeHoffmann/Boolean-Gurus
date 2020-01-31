b0b7618b568b18980cbddc999db8ec5324056857
Colby Sherwood <colbysherwood44@gmail.com>
2020-01-31 17:00:15 -0500

    Removed init call from putTable and changed name of instance in Table class

--- Project1/TableMechanics.rb
+++ Project1/TableMechanics.rb
@@ -1,20 +1,20 @@
 class Table
   def initialize
-	@table = Array.new
+	@currentTable = Array.new
 	for i in 0..11
-		@table.push Card.drawRandom
+		@currentTable.push Card.drawRandom
 	end
   end
 end
 
-def putTable
-	@currentTable = Table.new
-	@counter = 0
+def putTable(currentTable)
+	counter = 0
 	for i in 0..2
 		for j in 0..3
-			puts "#{@currentTable[@counter].shape}, #{@currentTable[@counter].color}, #{@currentTable[@counter].number}, #{@currentTable[@counter].pattern} "
-			@counter = @counter + 1	
+			puts "#{currentTable[counter].shape}, #{currentTable[counter].color}, #{currentTable[counter].number}, #{currentTable[counter].pattern} "
+			counter = counter + 1	
 		end
 		puts "\n" 
 	end
 end
+
