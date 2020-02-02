b222cbd897f765248ac7a6766d20191b7350e0b9
UberFood <59709698+UberFood@users.noreply.github.com>
2020-02-02 02:07:20 -0500

    Update setsOnTable.rb
    Accessing array of cards - private variable in Table class, requires fix

--- Project1/setsOnTable.rb
+++ Project1/setsOnTable.rb
@@ -1,10 +1,11 @@
 # this assumes certain arguments for isProperSet function and table being a linear array. Can be changed if needed.
+# currently I access class variable currentTable directly, which is impossible since it is private. Need an accessor function
 def setsOnTable (table)
   numSets = 0
   for i in 0...12
     for j in 0...i
       for k in 0...j
-        if (isProperSet(table[i], table[j], table[k]))
+        if (isProperSet(table.currentTable[i], table.currentTable[j], table.currentTable[k]))
           numSets = numSets + 1
         end
       end
