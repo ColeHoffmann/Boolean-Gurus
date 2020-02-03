ab5dafc4ede4d9255aa842a72adfd73eeba2b186
Yufei Pan <pan.712@osu.edu>
2020-02-02 21:28:19 -0500

    bug fixs and performance improvements in setsOnTable

--- Project1/setsOnTable.rb
+++ Project1/setsOnTable.rb
@@ -2,9 +2,9 @@
 # currently I access class variable currentTable directly, which is impossible since it is private. Need an accessor function
 def setsOnTable (table)
   numSets = 0
-  (0...12).each { |i|
-    (0...i).each { |j|
-      (0...j).each { |k|
+  (0..10).each { |i|
+    (i+1..11).each { |j|
+      (j+1..12).each { |k|
         if (isProperSet(table.currentTable[i], table.currentTable[j], table.currentTable[k]))
           numSets = numSets + 1
         end
@@ -20,30 +20,28 @@ end
 # @ensures: true if hand is proper set, false if not
 # @requires: card1, card2, and card3 are all card objects
 def isProperSet(card1, card2, card3)
-  valid = Array[3,6,9]
-  #we will assume it is proper set, we will then go through and check each attribute to make sure it follows the rule. 
-  properSet = true;
+  valid = [3,6,9]
 
 
   #unless the sum of color is contained in the valid array, make this attrubute is false.. 
   #all other attributes follow this convention
   unless (valid.include?(card1.color + card2.color + card3.color))
-    properSet = false
+    return false
   end
   
   unless (valid.include?(card1.shape + card2.shape + card3.shape))
-    properSet = false
+    return false
   end 
 
   unless (valid.include?(card1.pattern + card2.pattern + card3.pattern))
-    properSet = false
+    return false
   end
 
   unless (valid.include?(card1.number + card2.number + card3.number))
-    properSet = false
+    return false
   end
 
   #if any one of the sums is not 3, 6, or 9. Proper set will return false. 
   #return:
-  properSet
+  return true
 end
