1bb1d24a83dbd263c221bdd44db6880d1a7c4619
Yufei Pan <pan.712@osu.edu>
2020-02-02 19:32:29 -0500

    updated the .gitignore file for .idea/ file. And updated userTurn.rb for input checking

--- Project1/userTurn.rb
+++ Project1/userTurn.rb
@@ -2,27 +2,51 @@
 # divided into small parts in case we want to change behavior of some of them
 # designed so we call turn in a loop, puts current table, then updates score and table
 
+def validIn? (inStr)
+  if inStr.to_i == 0
+    puts "Invalid input!/n"
+    return false
+  end
+  return true
+end
+
 def turn(table, player)
   table.putTable(table.currentTable)
-  puts "Please pick your first card or enter no sets /n"
+  puts "Please pick your first card or enter \"no sets\" /n"
   s = gets
-  if (s.equal?("no sets")
-      noSetsOption(table, player)
-  else 
-      pickCardsOption(table, player)
+  until s.equal?("no sets") || validIn?(s)
+    puts "Please pick your first card or enter \"no sets\" /n"
+    s = gets
+  end
+  if (s.equal?("no sets"))
+    noSetsOption(table, player)
+  else
+    pickCardsOption(table, player,s.to_i)
   end
 end
 
-def pickCardsOption (table, player)
-  firstCardNum = s.to_i
+
+
+def pickCardsOption (table, player, firstCard)
+  firstCardNum = firstCard
   puts "Please pick your second card /n"
-  secondCardNum = gets.to_i
+  inStr = gets
+  until (validIn?(inStr))
+    puts "Please pick your second card /n"
+    inStr = gets
+  end
+  secondCardNum = inStr
+  inStr = gets
   puts "Please pick your third card /n"
-  thirdCardNum = gets.to_i
-  if (isProperSet(table.currentTable[firstCardNum], table.currentTable[secondCardNum], table.currentTable[thirdCardNum])
+  until (validIn?(inStr))
+    puts "Please pick your third card /n"
+    inStr = gets
+  end
+  thirdCardNum = inStr
+  if (isProperSet(table.currentTable[firstCardNum], table.currentTable[secondCardNum], table.currentTable[thirdCardNum]))
     pickedProper(table, player, firstCardNum, secondCardNum, thirdCardNum)
   else 
-    pickedUnproper(player)
+    pickedImproper(player)
   end  
 end
 
@@ -31,7 +55,7 @@ def pickedProper(table, player, firstCardNum, secondCardNum, thirdCardNum)
   table.changeCards(table.currentTable[firstCardNum], table.currentTable[secondCardNum], table.currentTable[thirdCardNum])
 end
 
-def pickedUnproper(player)
+def pickedImproper(player)
   player.decrease_score
   puts "Please try again"
 end
