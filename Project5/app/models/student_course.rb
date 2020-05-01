62183382d389a5a1b7953bd1df11c2ac988f5ee9
UberFood <akavets.1@osu.edu>
2020-05-01 19:06:59 -0400

    Added input validation for courses taken (course number and grade)

--- Project5/app/models/student_course.rb
+++ Project5/app/models/student_course.rb
@@ -1,4 +1,6 @@
 class StudentCourse < ApplicationRecord
 	belongs_to :user
+	validates_format_of :course_number, :with => /\A(\d{4})\z/
+	validates_format_of :grade, :with => /\A[A-F](\+|\-)?\z/
 
 end
